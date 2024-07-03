2669f70c71c7cee939eeb5be134ab727a5fa3984
Ammari045 <Ammari045@users.noreply.github.com>
2024-07-03 22:43:35 +0500

    login state added

--- src/store/user/userApi.js
+++ src/store/user/userApi.js
@@ -1,12 +1,13 @@
 // userApi.js
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import envVars from "../../../envexport";
 
 export const loginUser = createAsyncThunk(
     "user/login",
     async (credentials, { rejectWithValue }) => {
         try {
-            const response = await axios.post("/api/v1/user/login", credentials);
+            const response = await axios.post(`${envVars.backend_uri}/user/login`, credentials);
             return response.data;
         } catch (err) {
             return rejectWithValue(err.response.data);
