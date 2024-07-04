0f2bf0a957c30e689f4ea9bcf776932af8b0c1f9
Ammari045 <Ammari045@users.noreply.github.com>
2024-07-04 14:57:18 +0500

    logout functionality added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -90,7 +90,7 @@ function Navbar() {
                           </Link>
                         </li>
                       ))}
-                  <Logout />
+                  {user && <Logout />}
                 </ul>
               </div>
             </div>
