423ebe8fc20ef01d9d8cc3c5ff256f0b6c18926b
Ammari045 <Ammari045@users.noreply.github.com>
2024-07-05 09:44:14 +0500

    logout functionality added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../store/user/userAuthSlice";
 
 function Layout() {
+  const user = useSelector(selectUser);
   return (
     <div className="my-48">
-      <h1 className="bg-slate-800 p-4 text-center text-5xl font-bold text-white shadow-xl shadow-rose-700">
-        Click On The Book Shop To See Books
-      </h1>
+      {user ? (
+        <h1 className="bg-slate-800 p-4 text-center text-5xl font-bold text-white shadow-xl shadow-rose-700">
+          Click On The Book Shop To See Books
+        </h1>
+      ) : (
+        <h1 className="bg-slate-800 p-4 text-center text-5xl font-bold text-white shadow-xl shadow-rose-700">
+          Click On The Login To See Books
+        </h1>
+      )}
     </div>
   );
 }
