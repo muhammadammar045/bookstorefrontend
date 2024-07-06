490088ed5d48c5b8087876ca125eca7835271694
Ammari045 <Ammari045@users.noreply.github.com>
2024-07-06 10:26:16 +0500

    pagination middleware updated

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -28,7 +28,7 @@ function Logout() {
             className="block rounded px-3 py-2 md:p-0 dark:hover:text-orange-600 md:dark:text-white"
             aria-current="page"
           >
-            <PacmanLoader /> Logging Out
+            Logging Out...
           </button>
         </li>
       ) : (
