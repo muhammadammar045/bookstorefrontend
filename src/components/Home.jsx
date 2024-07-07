647e3a01412bfe1363146487073a58c3e2aa1c30
Ammari045 <Ammari045@users.noreply.github.com>
2024-07-07 14:19:12 +0500

    protected routes adjustments

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { selectUser } from "../store/user/userAuthSlice";
 import { useSelector } from "react-redux";
+import Typewriter from "typewriter-effect";
 
 function Home() {
   const user = useSelector(selectUser);
@@ -13,9 +14,18 @@ function Home() {
           </span>
         </h1>
       ) : (
-        <h1 className="p-4 text-center text-5xl font-bold text-white">
-          <span className="bg-slate-800 px-12 italic shadow-xl shadow-rose-700">
-            Login To Read,Add,Update and Delete Books
+        <h1 className="p-4 text-5xl font-bold text-white">
+          <span className="flex items-center justify-center bg-slate-800 px-12 italic shadow-xl shadow-rose-700">
+            <span> Login To </span>
+            <Typewriter
+              options={{
+                wrapperClassName: "text-red-400 ml-4",
+                strings: ["Read", "Add", "Update", "Delete"],
+                autoStart: true,
+                loop: true,
+              }}
+            />{" "}
+            <span className="ml-2"> Books</span>
           </span>
         </h1>
       )}
