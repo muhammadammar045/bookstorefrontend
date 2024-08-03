a71ef7687e4eda8512478ebe08bfcd884546361c
Ammari045 <Ammari045@users.noreply.github.com>
2024-08-03 16:50:58 +0500

    role and permission assigning done

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@tailwindConfig': path.resolve(__dirname, 'tailwind.config.js'),
-      '@admin': path.resolve(__dirname, 'src/Admin') // Alias for Admin folder
-    },
-  },
-  optimizeDeps: {
-    include: ['@tailwindConfig'],
-  },
-});
+
+})
